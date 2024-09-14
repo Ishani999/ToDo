1138b8ada7a570639ec87be4b6cb1bd4989d55bf
Ishani Anushka <ishanianushka.es@gmail.com>
2024-09-14 16:51:57 +0530

    UseAuth Update

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,12 +4,12 @@ import React, { createContext, useState, useContext } from 'react';
 // Create AuthContext
 const AuthContext = createContext();
 
-// AuthProvider Component to wrap the app
+// AuthProvider Component 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   const login = (email, password) => {
-    // Simulate login logic (could be extended with validation)
+    // Simulate login logic 
     setUser({ email });
   };
 
